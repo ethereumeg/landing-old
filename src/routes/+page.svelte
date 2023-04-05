73de5ec519a09c56c3da309c74de100a0d137eeb
tree <tree@hidden.cz>
2023-04-05 02:37:24 +0200

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
   import SvelteMarkdown from 'svelte-markdown'
 
   const manifesto = `
-In the sprawling landscape of the Infinite Garden, we stand as a group of nonconformist, working to cultivate and nurture the Ethereum community and their events. We are a diverse assembly of individuals from different fields and backgrounds, united by our passion for decentralized technologies and human coordination.
+In the sprawling landscape of the Infinite Garden, we stand as a group of nonconformist, working to cultivate and nurture the Ethereum community. We are a diverse assembly of individuals from different fields and backgrounds, united by our passion for decentralized technologies and human coordination.
 
 Our mission is to promote and support community events, and to foster a culture of inclusivity, diversity, and open participation. Embrace the unique local characteristics of every corner of the world, and celebrate the voluntary work that is done with meaning and purpose.
 
@@ -11,7 +11,7 @@ We stand in opposition to commercialization and unification, rejecting the notio
 
 As a group of cypherpunks, we cherish the basic principles of freedom and collaboration that underlie Ethereum. Our diverse membership, comprising both solarpunks and lunarpunks, brings a range of perspectives and energies to our mission. 
 
-Let us join hands and hearts. Let us sow the seeds of a vibrant and inclusive community, one that will bloom and flourish for generations to come.
+Let us sow the seeds of a vibrant and inclusive community, one that will bloom and flourish for generations to come.
 
 *Your Ethereum Gardeners*
 `
